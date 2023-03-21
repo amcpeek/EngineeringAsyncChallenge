162215fe0681b2c9da820e22a4212f9df28fe085
amcpeek <ammcpeek@gmail.com>
2023-03-20 18:53:44 -0700

    basic css structure

--- Front_End_Challenge/my-app/src/App.tsx
+++ Front_End_Challenge/my-app/src/App.tsx
@@ -1,24 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='center column'>
+      <div className='b-red column'>
+      <div>Logo</div>
+      <div>Better Lesson</div>
+      <div>Professional Coaching</div>
+      <div>Professional Coach Seminars & Mentorship</div>
+      <button>Register Now</button>
+      </div>
+
+      <div className='b-orange row center'>
+        <div className='left b-green'>img</div>
+        <div className='right b-blue'>
+          <div>Current Coaches</div>
+          <div>Coach Name, Available Starting, Industry</div>
+          <div>Jessica D. 11/6/22 Professional Services</div>
+          <div>David F. 8/5/21 Sports/Fitness</div>
+          <div>Keir Y. 4/12/22 E-Sports</div>
+        </div>
+      </div>
+      <div className='b-green center'>
+        <div>Join our mailing list</div>
+        <div className='b-red row'>
+          <div className='b-orange'>
+            Form:
+            Full Name
+            Email
+            Industry
+          </div>
+          <div className='b-green center'>
+            <div>Join our mailing to receive notifications about program availability and special discounts</div>
+            <button>Sign Up</button>
+
+          </div>
+        </div>
+      </div>
+      <div className='b-blue row center'>
+        <div>
+          <div>Email Address</div>
+          <div>hello@reallygreatsite.com</div>
+        </div>
+
+        <div>
+          <div>Mailing Address</div>
+          <div>123 Anywhere St. Any City, ST 12345</div>
+        </div>
+
+        <div>
+          <div>Phone Number</div>
+          <div>(123) 456-7890</div>
+        </div>
+      </div>
+
+
     </div>
   );
 }
