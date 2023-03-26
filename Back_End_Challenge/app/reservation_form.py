36cdade71df1b3b895ee18d06f6f3bf789f7d63b
amcpeek <ammcpeek@gmail.com>
2023-03-25 21:39:46 -0700

    a few small changes of comments and adding in the user name to backend, prior to solving the local time issue

--- Back_End_Challenge/app/reservation_form.py
+++ Back_End_Challenge/app/reservation_form.py
@@ -1,16 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField
 from wtforms.validators import DataRequired, ValidationError
 from datetime import datetime
 from app.models import Reservation
 
 
+# need to convert the start and ends times to local time
 def valid_startTime(form, field):
     startTime = datetime.strptime(field.data, "%Y-%m-%dT%H:%M:%SZ")
     current = datetime.now()
     # datetime.now() is automatically local time
     if startTime < current:
-        raise ValidationError('Start time can not be in the past.')
+        raise ValidationError('Start time can not be in the past for local time.')
 
 
 # validation the endTime is after the current time was not in the instructions, but I added it
@@ -18,7 +19,7 @@ def valid_endTime(form, field):
     endTime = datetime.strptime(field.data, "%Y-%m-%dT%H:%M:%SZ")
     current = datetime.now()
     if endTime < current:
-        raise ValidationError('End time can not be in the past.')
+        raise ValidationError('End time can not be in the past for local time.')
 
 
 class ReservationForm(FlaskForm):
@@ -29,7 +30,7 @@ class ReservationForm(FlaskForm):
     endTime = StringField('endTime', validators=[
                           DataRequired(), valid_endTime])
 
-    def validate_order(self, **kwargs):
+    def validate_order(self):
         # Standard validators
         rv = FlaskForm.validate(self)
         # Ensure all standard validators are met
@@ -48,7 +49,6 @@ class ReservationForm(FlaskForm):
 
                 oneRes = res.to_dict()
                 oneResStartString = str(oneRes['startTime'])
-                # should convert to local time, then compared to time the user submitted in their local time
                 compareStartTime = datetime.strptime(
                     oneResStartString, "%Y-%m-%dT%H:%M:%SZ")
                 oneResEndString = str(oneRes['endTime'])
@@ -56,24 +56,24 @@ class ReservationForm(FlaskForm):
                     oneResEndString, "%Y-%m-%dT%H:%M:%SZ")
 
                 compareUser = oneRes['user']
-                print('compareUser', compareUser, 'self.user', self.user.data)
+                #print('compareUser', compareUser, 'self.user', self.user.data)
                 # I decided to give very clear feedback to the user for them to try a better reservation time
                 if compareUser == self.user.data:
                     if startTime == compareStartTime and endTime == compareEndTime:
                         self.endTime.errors.append(
-                            f'''This user already has an event with that exact time block new: {startTime} == cur: {compareStartTime} and new: {endTime}  == cur: {compareEndTime} ''')
+                            f'''{compareUser} already has an event with that exact time block new: {startTime} == cur: {compareStartTime} and new: {endTime}  == cur: {compareEndTime} ''')
                         return False
                     if startTime < compareStartTime and endTime > compareEndTime:
                         self.endTime.errors.append(
-                            f'''This user already has an event that starts before and continues to after this time block new: {startTime}  < cur: {compareStartTime} and new: {endTime}  > cur: {compareEndTime} ''')
+                            f'''{compareUser} already has an event that starts before and continues to after this time block new: {startTime}  < cur: {compareStartTime} and new: {endTime}  > cur: {compareEndTime} ''')
                         return False
                     if startTime <= compareStartTime and endTime > compareStartTime:
                         self.endTime.errors.append(
-                            f'''This user already has an event overlapping with the start time, but not the end time new: {startTime}  <= cur: {compareStartTime} and new: {endTime}  > cur: {compareStartTime} ''')
+                            f'''{compareUser} already has an event overlapping with the start time, but not the end time new: {startTime}  <= cur: {compareStartTime} and new: {endTime}  > cur: {compareStartTime} ''')
                         return False
                     if endTime >= compareEndTime and startTime < compareEndTime:
                         self.endTime.errors.append(
-                            f'''This user already has an event overlapping with the end time, but not the start time  new: {endTime}  >= cur: {compareEndTime} and new: {startTime}  < cur: {compareEndTime} ''')
+                            f'''{compareUser} already has an event overlapping with the end time, but not the start time  new: {endTime}  >= cur: {compareEndTime} and new: {startTime}  < cur: {compareEndTime} ''')
                         return False
 
             return True
